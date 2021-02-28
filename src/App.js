e14a4a3f32c324a16dbb742e99634d4fe89ad6df
Abdul Rauf <abdul.rauf@renovite.com>
2021-02-28 12:02:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="app-header">
+          <h2>
+            Notes & Todo's
+          </h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
